13dbd0dc57e04ba833b3a2fb52db6dbcbe6440bc
SnyderConsulting <andrew.snyder@snyderconsulting.tech>
2022-12-01 11:26:54 -0600

    Complete Day 1 Challenge

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,42 @@
 fun main() {
+    fun getElvesList(input: List<String>): List<List<Int>> {
+        val elves = mutableListOf(mutableListOf<Int>())
+        var counter = 0
+
+        input.forEach {
+            if (it.isBlank()) {
+                counter += 1
+                elves.add(mutableListOf())
+            } else {
+                elves[counter].add(it.toInt())
+            }
+        }
+
+        return elves
+    }
+
     fun part1(input: List<String>): Int {
-        return input.size
+        val elves = getElvesList(input)
+
+        return elves
+            .maxBy { elf ->
+                elf.sumOf { calorie -> calorie }
+            }.sumOf { calorie -> calorie }
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val elves = getElvesList(input)
+
+        return elves
+            .sortedByDescending { elf ->
+                elf.sumOf { calorie -> calorie }
+            }
+            .take(3)
+            .sumOf { it.sum() }
     }
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
     val input = readInput("Day01")
+
     println(part1(input))
     println(part2(input))
 }
